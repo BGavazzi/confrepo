532607ed1863c574385b790c6881e99c82e0315b
Marcos Leal <marcosfasleal@gmail.com>
2012-05-17 14:41:57 -0300

    Cria branch para funcionar no mac

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 ln -s ~/confrepo/.vim ~/.
 ln -s ~/confrepo/.amazonrc ~/.
-rm ~/.bashrc
-ln -s ~/confrepo/.bashrc ~/.
+rm ~/.bash_profile
+ln -s ~/confrepo/.bashrc ~/.bash_profile
 ln -s ~/confrepo/.coderc ~/.
 ln -s ~/confrepo/.git-completion.bash ~/.
 ln -s ~/confrepo/.gitconfig ~/.
 ln -s ~/confrepo/.vimrc ~/.
 
-source ~/.bashrc
+source ~/.bash_profile
